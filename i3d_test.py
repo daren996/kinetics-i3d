23401f7c21ba416f4ba7a37c543d7a71be0bbfe9
daren996 <850002098@qq.com>
2019-11-21 23:42:43 -0500

    nothing

--- i3d_test.py
+++ i3d_test.py
@@ -28,65 +28,65 @@ _NUM_CLASSES = 400
 
 
 class I3dTest(tf.test.TestCase):
-  """Test of Inception I3D model, without real data."""
+    """Test of Inception I3D model, without real data."""
 
-  def testModelShapesWithSqueeze(self):
-    """Test shapes after running some fake data through the model."""
-    i3d_model = i3d.InceptionI3d(
-        num_classes=_NUM_CLASSES, final_endpoint='Predictions')
-    inp = tf.placeholder(tf.float32, [None, 64, _IMAGE_SIZE, _IMAGE_SIZE, 3])
-    predictions, end_points = i3d_model(
-        inp, is_training=True, dropout_keep_prob=0.5)
+    def testModelShapesWithSqueeze(self):
+        """Test shapes after running some fake data through the model."""
+        i3d_model = i3d.InceptionI3d(
+            num_classes=_NUM_CLASSES, final_endpoint='Predictions')
+        inp = tf.placeholder(tf.float32, [None, 64, _IMAGE_SIZE, _IMAGE_SIZE, 3])
+        predictions, end_points = i3d_model(
+            inp, is_training=True, dropout_keep_prob=0.5)
 
-    init_op = tf.global_variables_initializer()
-    with tf.Session() as sess:
-      sess.run(init_op)
-      sample_input = np.zeros((5, 64, _IMAGE_SIZE, _IMAGE_SIZE, 3))
-      out_predictions, out_logits = sess.run(
-          [predictions, end_points['Logits']], {inp: sample_input})
-      self.assertEqual(out_predictions.shape, (5, _NUM_CLASSES))
-      self.assertEqual(out_logits.shape, (5, _NUM_CLASSES))
+        init_op = tf.global_variables_initializer()
+        with tf.Session() as sess:
+            sess.run(init_op)
+            sample_input = np.zeros((5, 64, _IMAGE_SIZE, _IMAGE_SIZE, 3))
+            out_predictions, out_logits = sess.run(
+                [predictions, end_points['Logits']], {inp: sample_input})
+            self.assertEqual(out_predictions.shape, (5, _NUM_CLASSES))
+            self.assertEqual(out_logits.shape, (5, _NUM_CLASSES))
 
-  def testModelShapesWithoutSqueeze(self):
-    """Test that turning off `spatial_squeeze` changes the output shape.
+    def testModelShapesWithoutSqueeze(self):
+        """Test that turning off `spatial_squeeze` changes the output shape.
 
     Also try setting different values for `dropout_keep_prob` and snt.BatchNorm
     `is_training`.
     """
-    i3d_model = i3d.InceptionI3d(
-        num_classes=_NUM_CLASSES, spatial_squeeze=False,
-        final_endpoint='Predictions')
-    inp = tf.placeholder(tf.float32, [None, 64, _IMAGE_SIZE, _IMAGE_SIZE, 3])
-    predictions, end_points = i3d_model(
-        inp, is_training=False, dropout_keep_prob=1.0)
+        i3d_model = i3d.InceptionI3d(
+            num_classes=_NUM_CLASSES, spatial_squeeze=False,
+            final_endpoint='Predictions')
+        inp = tf.placeholder(tf.float32, [None, 64, _IMAGE_SIZE, _IMAGE_SIZE, 3])
+        predictions, end_points = i3d_model(
+            inp, is_training=False, dropout_keep_prob=1.0)
 
-    init_op = tf.global_variables_initializer()
-    with tf.Session() as sess:
-      sess.run(init_op)
-      sample_input = np.zeros((5, 64, _IMAGE_SIZE, _IMAGE_SIZE, 3))
-      out_predictions, out_logits = sess.run(
-          [predictions, end_points['Logits']], {inp: sample_input})
-      self.assertEqual(out_predictions.shape, (5, 1, 1, _NUM_CLASSES))
-      self.assertEqual(out_logits.shape, (5, 1, 1, _NUM_CLASSES))
+        init_op = tf.global_variables_initializer()
+        with tf.Session() as sess:
+            sess.run(init_op)
+            sample_input = np.zeros((5, 64, _IMAGE_SIZE, _IMAGE_SIZE, 3))
+            out_predictions, out_logits = sess.run(
+                [predictions, end_points['Logits']], {inp: sample_input})
+            self.assertEqual(out_predictions.shape, (5, 1, 1, _NUM_CLASSES))
+            self.assertEqual(out_logits.shape, (5, 1, 1, _NUM_CLASSES))
 
-  def testInitErrors(self):
-    # Invalid `final_endpoint` string.
-    with self.assertRaises(ValueError):
-      _ = i3d.InceptionI3d(
-          num_classes=_NUM_CLASSES, final_endpoint='Conv3d_1a_8x8')
+    def testInitErrors(self):
+        # Invalid `final_endpoint` string.
+        with self.assertRaises(ValueError):
+            _ = i3d.InceptionI3d(
+                num_classes=_NUM_CLASSES, final_endpoint='Conv3d_1a_8x8')
 
-    # Dropout keep probability must be in (0, 1].
-    i3d_model = i3d.InceptionI3d(num_classes=_NUM_CLASSES)
-    inp = tf.placeholder(tf.float32, [None, 64, _IMAGE_SIZE, _IMAGE_SIZE, 3])
-    with self.assertRaises(ValueError):
-      _, _ = i3d_model(inp, is_training=False, dropout_keep_prob=0)
+        # Dropout keep probability must be in (0, 1].
+        i3d_model = i3d.InceptionI3d(num_classes=_NUM_CLASSES)
+        inp = tf.placeholder(tf.float32, [None, 64, _IMAGE_SIZE, _IMAGE_SIZE, 3])
+        with self.assertRaises(ValueError):
+            _, _ = i3d_model(inp, is_training=False, dropout_keep_prob=0)
 
-    # Height and width dimensions of the input should be _IMAGE_SIZE.
-    i3d_model = i3d.InceptionI3d(num_classes=_NUM_CLASSES)
-    inp = tf.placeholder(tf.float32, [None, 64, 10, 10, 3])
-    with self.assertRaises(ValueError):
-      _, _ = i3d_model(inp, is_training=False, dropout_keep_prob=0.5)
+        # Height and width dimensions of the input should be _IMAGE_SIZE.
+        i3d_model = i3d.InceptionI3d(num_classes=_NUM_CLASSES)
+        inp = tf.placeholder(tf.float32, [None, 64, 10, 10, 3])
+        with self.assertRaises(ValueError):
+            _, _ = i3d_model(inp, is_training=False, dropout_keep_prob=0.5)
 
 
 if __name__ == '__main__':
-  tf.test.main()
+    tf.test.main()
